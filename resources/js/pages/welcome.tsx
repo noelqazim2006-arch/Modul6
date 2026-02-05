682664493f2ef866c3d2cc5fca8e57257f542069
Noel.qazim <noel.qazim2006@gmail.com>
2026-02-05 12:55:25 +0100

    initialize project structure with configuration files and basic setup

---
+++ resources/js/pages/welcome.tsx
@@ -0,0 +1,11 @@
+import { Head } from '@inertiajs/react';
+
+export default function Welcome() {
+return (
+    <>
+    <Head title="Welcome"></Head>
+    <p> This is my Page</p>
+    </>
+)
+    
+}
